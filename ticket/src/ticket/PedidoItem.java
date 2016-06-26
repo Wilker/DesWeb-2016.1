d9a230404fedfe9e8ce80e3f614e8109447a7e38
Wilker <Wilker.rj@gmail.com>
2016-06-25 23:51:20 -0300

    Implementado AbstratctRecord na classe PedidoItem. Alterado atributo produto para referenciar a chave da entidade Produto

--- ticket/src/ticket/PedidoItem.java
+++ ticket/src/ticket/PedidoItem.java
@@ -3,46 +3,73 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ticket;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 /**
  *
  * @author renan.vieira
  */
-public class PedidoItem {
-    
-    private Produto produto;
+public class PedidoItem extends AbstractRecord {
+
+    private static DBSimulator<PedidoItem> db = new DBSimulator<>();
+
+    private int produto; //alterado para guarda a referência do produto
     private int quantidade;
     private long valorvendido;
     private long desconto;
-    
-    public Produto getProduto(){
-       return produto;
+
+    // <editor-fold desc="Getters" defaultstate="collapsed">
+    public int getProduto() {
+        return produto;
     }
-    public void setProduto(Produto v){
-       produto = v;
-    } 
-    
-    public int getQuantidade(){
-       return quantidade;
+
+    public void setProduto(int idProduto) {
+        produto = idProduto;
     }
-    public void setQuantidade(int v){
-       quantidade = v;
+
+    public int getQuantidade() {
+        return quantidade;
     }
-      
-    public long getValorVendido(){
-       return valorvendido;
+
+    public void setQuantidade(int v) {
+        quantidade = v;
     }
-    public void setValorVendido(long v){
-       valorvendido = v;
+
+    public long getValorVendido() {
+        return valorvendido;
     }
-    
-    public long getDesconto(){
-       return desconto;
+
+    public void setValorVendido(long v) {
+        valorvendido = v;
     }
-    public void setDesconto(long v){
-       desconto = v;
+
+    public long getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(long v) {
+        desconto = v;
+    }
+// </editor-fold>
+
+    @Override
+    DBSimulator getDB() {
+        return PedidoItem.db;
+    }
+
+    public static PedidoItem getById(int id) {
+        return PedidoItem.db.getById(id);
+    }
+
+    public static List<PedidoItem> selectAll() {
+        return PedidoItem.db.selectAll();
+    }
+
+    public static List<PedidoItem> where(Predicate<PedidoItem> clause) {
+        return PedidoItem.db.where(clause);
     }
     
 }
