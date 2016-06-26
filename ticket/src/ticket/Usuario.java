f0cf39bccffacfa6dd22f34c311d7b4af0a658ab
Wilker <Wilker.rj@gmail.com>
2016-06-26 01:05:45 -0300

    Implementada classe usuário para estender Abstratct Record

--- ticket/src/ticket/Usuario.java
+++ ticket/src/ticket/Usuario.java
@@ -3,60 +3,81 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package ticket;
+
+import java.util.List;
+import java.util.function.Predicate;
 
 /**
  *
  * @author renan.vieira
  */
-public class Usuario {
-    
-    private int usuarioid;
+public class Usuario extends AbstractRecord {
+
+    private static DBSimulator<Usuario> db = new DBSimulator<>();
+
+    private int usuarioId;
     private String nome;
     private String login;
     private String senha;
     private String email;
-    
-    public int getUsuarioId(){
-       return usuarioid;
+
+    // <editor-fold desc="Getters e Setters" defaultstate="collapsed">
+    public int getUsuarioId() {
+        return usuarioId;
     }
-    
-    public void setUsuarioId(int v){
-       usuarioid = v;
+
+    public void setUsuarioId(int v) {
+        usuarioId = v;
     }
-    
-    public String getNome(){
-       return nome;
+
+    public String getNome() {
+        return nome;
     }
-    
-    public void setNome(String v){
-       nome = v;
+
+    public void setNome(String v) {
+        nome = v;
     }
-    
-    public String getLogin(){
-       return login;
+
+    public String getLogin() {
+        return login;
     }
-    
-    public void setLogin(String v){
-       login = v;
+
+    public void setLogin(String v) {
+        login = v;
     }
-    
-    public String getSenha(){
-       return senha;
+
+    public String getSenha() {
+        return senha;
     }
-    
-    public void setSenha(String v){
-       senha = v;
+
+    public void setSenha(String v) {
+        senha = v;
     }
-    
-    public String getEmail(){
-       return email;
+
+    public String getEmail() {
+        return email;
     }
-    
-    public void setEmail(String v){
-       email = v;
+
+    public void setEmail(String v) {
+        email = v;
     }
-    
-    
+    // </editor-fold>
+
+    @Override
+    DBSimulator getDB() {
+        return Usuario.db;
+    }
+
+    public static Usuario getById(int id) {
+        return Usuario.db.getById(id);
+    }
+
+    public static List<Usuario> selectAll() {
+        return Usuario.db.selectAll();
+    }
+
+    public static List<Usuario> where(Predicate<Usuario> clause) {
+        return Usuario.db.where(clause);
+    }
 }
