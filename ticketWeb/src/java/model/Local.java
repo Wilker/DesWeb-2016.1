7346471836c4a55328f28d18088156d047204e1e
Wilker <Wilker.rj@gmail.com>
2016-07-21 21:54:12 -0300

    Adicionada Classe Local

---
+++ ticketWeb/src/java/model/Local.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author wilker
+ */
+@Entity
+@Table(name = "TICKET.LOCAL")
+public class Local {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @Column(name = "LOGRADOURO")
+    private String logradouro;
+
+    @Column(name = "NUMERO")
+    private String numero;
+
+    @Column(name = "BAIRRO")
+    private String bairro;
+
+    @Column(name = "CEP")
+    private String cep;
+
+    @Column(name = "CIDADE")
+    private String cidade;
+
+    public Local() {
+    }
+
+    public Local(String nome, String logradouro, String numero, String bairro, String cep, String cidade) {
+        super();
+        this.nome = nome;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cidade = cidade;
+    }
+
+    // <editor-fold desc="Getters" defaultstate="collapsed">
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+}
+// </editor-fold>
+
